4a59e06cc4fd28d9a9f4293064e0d131af02977a
Merculous <24739823+Merculous@users.noreply.github.com>
2025-01-09 20:10:04 -0600

    Ok try fixing this again.

--- armfind/find.py
+++ armfind/find.py
@@ -21,12 +21,12 @@ def searchForInsn(data: Buffer, offset: Index, insn: Any, insnBitSizes: InsnBitS
     if insnSize not in (2, 4):
         raise Exception(f'Instruction size is not 2 or 4!')
 
-    searchStart = offset & ~1
+    searchStart = offset & ~(insnSize - 1)
     searchEnd = len(data)
     match = None
     table = {}
 
-    for i in range(searchStart, searchEnd, 2):
+    for i in range(searchStart, searchEnd, insnSize):
         buffer = getBufferAtIndex(data, i, insnSize)
 
         if buffer in table:
